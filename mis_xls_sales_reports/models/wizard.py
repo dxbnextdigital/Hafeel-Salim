09d3cc643ee727c011da38480b8149cf4348f8b6
mindinfosys <67480042+mindinfosys@users.noreply.github.com>
2021-03-24 11:57:42 +0400

    update

--- mis_xls_sales_reports/models/wizard.py
+++ mis_xls_sales_reports/models/wizard.py
@@ -113,9 +113,6 @@ class SalesReport(models.TransientModel):
         wbf['content_int_border_total'].set_right()
 
 
-
-
-
         objsalesln = self.env['sale.order.line'].search([('order_id', 'in', salsids)],order='id')
 
         rowno = 1
@@ -136,6 +133,7 @@ class SalesReport(models.TransientModel):
         sheet.set_column(colno, colno, column_width)
         sheet.write(rowno - 1, colno, 'Sales Person', wbf['content_border_bg'])
 
+
         colno += 1
         column_width = 35
         sheet.set_column(colno, colno, column_width)
@@ -155,6 +153,12 @@ class SalesReport(models.TransientModel):
         column_width = 65
         sheet.set_column(colno, colno, column_width)
         sheet.write(rowno-1, colno, 'Product', wbf['content_border_bg'])
+
+        colno += 1
+        column_width = 20
+        sheet.set_column(colno, colno, column_width)
+        sheet.write(rowno - 1, colno, 'Analytic Tag', wbf['content_border_bg'])
+
 
         colno += 1
         column_width = 10
@@ -191,7 +195,41 @@ class SalesReport(models.TransientModel):
         sheet.set_column(colno, colno, column_width)
         sheet.write(rowno - 1, colno, 'Total Profit', wbf['content_border_bg'])
 
+        summary_sales_person = {}
+        summary_brand = {}
+        summary_partner = {}
         for rec in objsalesln:
+
+            if (rec.salesman_id.name in summary_sales_person):
+                tmptotal = float(summary_sales_person[rec.salesman_id.name]) + rec.price_subtotal
+                summary_sales_person[rec.salesman_id.name] = tmptotal
+            else:
+                summary_sales_person[rec.salesman_id.name] = rec.price_subtotal
+
+            #################### Partner
+
+            if (rec.order_id.partner_id.id in summary_partner):
+                dic_partner = summary_partner[rec.order_id.partner_id.id]
+                dic_partner['totalsale']=dic_partner['totalsale']+rec.price_subtotal
+                summary_partner[rec.order_id.partner_id.id] = dic_partner
+            else:
+                dic_partner={}
+                dic_partner= {'partnername': rec.order_id.partner_id.name, 'totalsale': rec.price_subtotal}
+                summary_partner[rec.order_id.partner_id.id] = dic_partner
+
+
+            ################## Brand
+            if (rec.product_id.brand.name in summary_brand):
+                dic_brand = summary_brand[rec.product_id.brand.name]
+                dic_brand['totalsale']=dic_brand['totalsale']+rec.price_subtotal
+                dic_brand['noofqty'] = dic_brand['noofqty'] +rec.product_uom_qty
+                #tmptotal = float(summary_brand[rec.product_id.brand.name]) + rec.price_subtotal
+                summary_brand[rec.product_id.brand.name] = dic_brand #tmptotal
+            else:
+                dic_brand={}
+                dic_brand= {'totalsale': rec.price_subtotal, 'noofqty': rec.product_uom_qty}
+                summary_brand[rec.product_id.brand.name] = dic_brand #rec.price_subtotal
+
 
             colno = 0
             sheet.write(rowno, colno, rowno, wbf['content_border'])
@@ -207,6 +245,13 @@ class SalesReport(models.TransientModel):
             sheet.write(rowno, colno, str(rec.product_id.barcode if rec.product_id.barcode else ""), wbf['content_border'])
             colno += 1
             sheet.write(rowno, colno, rec.product_id.name if rec.product_id.name else "", wbf['content_border'])
+
+            colno += 1
+            tagname=''
+            for analytic_tag in rec.analytic_tag_ids:
+                tagname += analytic_tag.name
+
+            sheet.write(rowno, colno, tagname , wbf['content_border'])
 
             colno += 1
             sheet.write(rowno, colno, rec.product_id.standard_price, wbf['content_float_border'])
@@ -229,16 +274,119 @@ class SalesReport(models.TransientModel):
             totqty=0
 
             rowno+=1
-        sheet.merge_range(rowno, 0, rowno, 9, "Total", wbf['content_border_bg'])
-        colno = 10
-        sheet.write(rowno, colno, "=sum(K2:K"+str(rowno)+")", wbf['content_int_border_total'])
+        sheet.merge_range(rowno, 0, rowno, 10, "Total", wbf['content_border_bg'])
         colno = 11
-        sheet.write(rowno, colno, "=sum(L2:L" + str(rowno) + ")", wbf['content_float_border_total'])
+        sheet.write(rowno, colno, "=sum(L2:L"+str(rowno)+")", wbf['content_int_border_total'])
         colno = 12
         sheet.write(rowno, colno, "=sum(M2:M" + str(rowno) + ")", wbf['content_float_border_total'])
         colno = 13
-        sheet.write(rowno, colno, "=sum(M2:M" + str(rowno) + ")", wbf['content_float_border_total'])
+        sheet.write(rowno, colno, "=sum(N2:N" + str(rowno) + ")", wbf['content_float_border_total'])
+        colno = 14
+        sheet.write(rowno, colno, "=sum(O2:O" + str(rowno) + ")", wbf['content_float_border_total'])
         rowno += 1
+
+        # summary_by_salesperson = {}
+        # summary_by_customer = {}
+        # for so in objsales:
+
+
+        ######################### Sales Person
+        worksheet2 = workbook.add_worksheet('Sales Person Summary')
+
+        colno = 0
+        column_width = 30
+        worksheet2.set_column(colno, colno, column_width)
+        worksheet2.write(0, colno, 'Sales Person Name', wbf['content_border_bg'])
+
+
+        colno += 1
+        column_width = 50
+        worksheet2.set_column(colno, colno, column_width)
+        worksheet2.write(0, colno, 'Total Sales', wbf['content_border_bg'])
+
+        rowno = 1
+        colno = 0
+        for recsale in summary_sales_person:
+            colno = 0
+            worksheet2.write(rowno, colno, recsale, wbf['content_border'])
+            colno += 1
+            worksheet2.write(rowno, colno, summary_sales_person[recsale], wbf['content_float_border'])
+            rowno += 1
+
+        worksheet2.write(rowno, 0,  "Total", wbf['content_border_bg'])
+        colno = 1
+        worksheet2.write(rowno, colno, "=sum(B2:B" + str(rowno) + ")", wbf['content_float_border_total'])
+
+
+        #######################################
+        ########################Brand summary_brand[rec.product_id.brand.name]
+        worksheet3 = workbook.add_worksheet('Brand Summary')
+
+        colno = 0
+        column_width = 50
+        worksheet3.set_column(colno, colno, column_width)
+        worksheet3.write(0, colno, 'Brand', wbf['content_border_bg'])
+
+        colno += 1
+        column_width = 30
+        worksheet3.set_column(colno, colno, column_width)
+        worksheet3.write(0, colno, 'No of Qty', wbf['content_border_bg'])
+
+        colno += 1
+        column_width = 30
+        worksheet3.set_column(colno, colno, column_width)
+        worksheet3.write(0, colno, 'Total Sales', wbf['content_border_bg'])
+
+        rowno = 1
+        colno = 0
+        for recbrand in summary_brand:
+            colno = 0
+            worksheet3.write(rowno, colno, recbrand if recbrand else '', wbf['content_border'])
+            colno += 1
+            dic_sales = summary_brand[recbrand]
+            worksheet3.write(rowno, colno, dic_sales['noofqty'], wbf['content_float_border'])
+            colno += 1
+
+            worksheet3.write(rowno, colno, dic_sales['totalsale'], wbf['content_float_border'])
+            rowno += 1
+        worksheet3.write(rowno, 0, "Total", wbf['content_border_bg'])
+        colno = 1
+        worksheet3.write(rowno, colno, "=sum(B2:B" + str(rowno) + ")", wbf['content_float_border_total'])
+        colno += 1
+        worksheet3.write(rowno, colno, "=sum(C2:C" + str(rowno) + ")", wbf['content_float_border_total'])
+
+        ##############################################
+
+        ########################Partner sales
+        worksheet4 = workbook.add_worksheet('Customer Sales Summary')
+
+        colno = 0
+        column_width = 50
+        worksheet4.set_column(colno, colno, column_width)
+        worksheet4.write(0, colno, 'Customer Name', wbf['content_border_bg'])
+
+
+        colno += 1
+        column_width = 30
+        worksheet4.set_column(colno, colno, column_width)
+        worksheet4.write(0, colno, 'Total Sales', wbf['content_border_bg'])
+
+        rowno = 1
+        colno = 0
+        for recpartner in summary_partner:
+            dic_partners = summary_partner[recpartner]
+            colno = 0
+            worksheet4.write(rowno, colno, dic_partners['partnername'] if dic_partners['partnername'] else '', wbf['content_border'])
+            colno += 1
+            worksheet4.write(rowno, colno, dic_partners['totalsale'], wbf['content_float_border'])
+            rowno += 1
+        worksheet4.write(rowno, 0, "Total", wbf['content_border_bg'])
+        colno = 1
+        worksheet4.write(rowno, colno, "=sum(B2:B" + str(rowno) + ")", wbf['content_float_border_total'])
+
+
+        ##############################################
+
 
         workbook.close()
         out = base64.encodestring(fp.getvalue())
