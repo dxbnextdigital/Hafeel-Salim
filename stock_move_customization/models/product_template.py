ee63e98c324f4f55c1efa6a2c0fb21d1abf86d64
mindinfosys <67480042+mindinfosys@users.noreply.github.com>
2021-03-31 00:20:16 +0400

    update

--- stock_move_customization/models/product_template.py
+++ stock_move_customization/models/product_template.py
@@ -4,6 +4,17 @@ class MisStockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
     partner_id = fields.Many2one('res.partner', related='move_id.partner_id', stored=True)
-    price_unit = fields.Float(related='move_id.price_unit', stored=True)
+    price_unit = fields.Float(compute='_compute_price_unit', stored=True)
     cost_price = fields.Float(related='product_id.standard_price', stored=True)
+
+    def _compute_price_unit(self):
+        for rec in self:
+            if rec.move_id.purchase_line_id:
+                rec.price_unit = rec.move_id.purchase_line_id.price_unit
+            elif rec.move_id.sale_line_id:
+                rec.price_unit = rec.move_id.sale_line_id.price_unit
+            elif rec.move_id.price_unit:
+                rec.price_unit = rec.move_id.price_unit
+            else:
+                rec.price_unit = rec.product_id.list_price
 
