054a9b39a13a4850163ec89cc79e71c812953bfc
mindinfosys <67480042+mindinfosys@users.noreply.github.com>
2021-04-11 22:56:59 +0400

    update

--- partner_credit_limit/models/partner.py
+++ partner_credit_limit/models/partner.py
@@ -1,9 +1,8 @@
 # See LICENSE file for full copyright and licensing details.
-
 from odoo import fields, models
-
-
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     over_credit = fields.Boolean('Allow Over Credit?', default=True)
+    block_nonpayment = fields.Boolean('Block for Non Payment?', default=False)
+    print_name = fields.Char(string='Print Name', required=True)
