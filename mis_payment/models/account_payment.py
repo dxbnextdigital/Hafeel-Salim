46b190e174ab99ad1f78484de4268c7969c43ae4
mindinfosys <67480042+mindinfosys@users.noreply.github.com>
2021-04-27 08:51:01 +0400

    update

--- mis_payment/models/account_payment.py
+++ mis_payment/models/account_payment.py
@@ -9,4 +9,24 @@ class account_payment(models.Model):
     def do_print_payment(self):
          return self.env.ref('account.action_report_payment_receipt').report_action(self)
 
+class AccountPaymentRegister(models.TransientModel):
+    _inherit = 'account.payment.register'
+    _description = 'Register Payment'
+
+    def _filterjournal(self):
+
+        if self.env.user.journal_id:
+            listids=self.env.user.journal_id.ids
+            return "[('company_id', '=', company_id),('type', 'in', ('bank', 'cash')), ('id', 'in', ("+ str(listids) +"))]"
+        else:
+            return "[('company_id', '=', company_id),('type', 'in', ('bank', 'cash'))]"
+
+    journal_id = fields.Many2one('account.journal', store=True, readonly=False,
+                                 compute='_compute_journal_id',
+                                 domain=_filterjournal)
+
+
+
+
+
 
