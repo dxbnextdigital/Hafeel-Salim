7e39a097b4315e4c9904bba9ae8d9a5c20f4b3a8
Hafeel Mohamed Salih <hafeel@asdxb.ae>
2022-06-07 11:11:19 +0400

    update

---
+++ imei_new_update/__manifest__.py
@@ -0,0 +1,22 @@
+
+
+{
+    'name': 'New imei update',
+    'version': '14.0.1.0.0',
+    'summary': 'Hide Any Menu Item User Wise',
+    'description': 'Hide Any Menu Item User Wise, Hide Menu Items, Hide Menu',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['sale_management','sale'],
+    'data': [
+'views/sale_order_form.xml',
+'views/new_update_imei_wizard.xml'
+    ],
+    'license': 'LGPL-3',
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ invoice_restriction_on_billing/__manifest__.py
@@ -0,0 +1,21 @@
+
+
+{
+    'name': 'Billing Group Settings',
+    'version': '14.0.1.0.0',
+    'summary': 'Hide Any Menu Item User Wise',
+    'description': 'Hide Any Menu Item User Wise, Hide Menu Items, Hide Menu',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['account'],
+    'data': [
+'views/account_account.xml'
+    ],
+    'license': 'LGPL-3',
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
