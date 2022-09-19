fbb9bde7fe2a5640e235060b31a417a678be047c
Techfuge <105791931+erpsupporttecfuge@users.noreply.github.com>
2022-09-18 21:57:59 -0400

    Add files via upload

---
+++ sale_order_restriction/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Sale Order Record Restriction",
+    "version": "14.0.1.0.1",
+    "category": "",
+    "website": "",
+    "depends": ['sale'],
+    "data": [
+        'security/invoice_invisible_group.xml',
+        'views/sale_order.xml'],
+    "installable": True,
+}
