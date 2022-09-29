a33f60d723f570b3a1111603fb1d4cf89216a4ba
Techfuge <105791931+erpsupporttecfuge@users.noreply.github.com>
2022-09-29 08:17:45 -0400

    Add files via upload

---
+++ inventory_report_total_qty/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Inventory Report With Total Quantity",
+    "version": "14.0.1.0.1",
+    "category": "",
+    "website": "",
+    "depends": ['stock'],
+    "data": [
+        'report/delivery_report.xml'
+        ],
+    "installable": True,
+}
