76d1bbfa4981e66ad64e427d0a20301eee294bb8
Techfuge <105791931+erpsupporttecfuge@users.noreply.github.com>
2022-11-03 00:17:33 -0400

    ADD IMEI IN SALE ORDER,INVOICE IN REPORT

---
+++ imei_number_report/__manifest__.py
@@ -0,0 +1,17 @@
+
+
+{
+    'name': 'IMEI TRACKER Receipt',
+    'version': '14.0.1.0.0',
+    'summary': '',
+    'description': '',
+    'depends': ['imei_number_trackers','sale','account'],
+    'data': [
+'report/sale_order.xml',
+        'report/account_move.xml'
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ imei_number_report/models/sale_order.py
@@ -0,0 +1,36 @@
+from odoo import fields, models, api
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def get_details_imei(self):
+        data ={}
+
+        for rec in self.imei_numbers_id:
+            data[rec.product_id.name] = []
+        for rec in self.imei_numbers_id:
+            temp =data[rec.product_id.name]
+            temp.append(rec.name)
+            data[rec.product_id.name] = temp
+        new_list = []
+        for key, val in data.items():
+            new_list.append([key, val])
+        return new_list
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+    sale_id = fields.Many2one('sale.order',compute='_compute_sale_order')
+
+    def _compute_sale_order(self):
+        temp = self.env['sale.order'].search([('invoice_ids', 'in', [self.id])],limit=1)
+        self.sale_id =temp
+
+
+    def get_data_from(self):
+        print(self.id)
+        invoice_id = self.id
+        temp = self.env['sale.order'].search([('invoice_ids', 'in', [invoice_id])],limit=1)
+        print(temp)
+
+        return temp.get_details_imei()
