10c517985e0eacd671127912e83d24e409573c36
Techfuge <105791931+erpsupporttecfuge@users.noreply.github.com>
2022-11-06 23:46:32 -0500

    Add files via upload

---
+++ change_group_on_payment_block/__manifest__.py
@@ -0,0 +1,18 @@
+
+
+{
+    'name': 'Enable Non Payment Block',
+    'version': '14.0.1.0.0',
+    'summary': '',
+    'description': '',
+    'depends': ['partner_customization'],
+    'data': [
+        'security/res_partner.xml',
+'views/res_partner.xml'
+
+    ],
+    'qweb': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
