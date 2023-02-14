bcc1da3b1da8019d4c9dfe6696e6f90045d586b8
Techfuge <105791931+erpsupporttecfuge@users.noreply.github.com>
2023-02-13 21:39:39 -0500

    Readonly sale

---
+++ read_only_sales_record/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Readonly Sales",
+    "version": "14.0.1.0.1",
+    "category": "",
+    "website": "",
+    "depends": ["sale","account_asset","account","purchase","account_reports","imei_number_trackers"],
+    "data": [
+        'security/readonly_sales_group.xml',
+        'security/ir.model.access.csv'
+
+    ],
+    "installable": True,
+}
