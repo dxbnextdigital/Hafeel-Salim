7ceadce5586c672a248109db67f313b3c1853f15
Techfuge <105791931+erpsupporttecfuge@users.noreply.github.com>
2023-07-14 21:06:01 +0530

    [added] user restriction based location

---
+++ user_specific_source_location/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "User Specific Source Location",
+    "version": "14.0.1.0.1",
+    "category": "",
+    "website": "",
+    "depends": ['sales_source_location'],
+    "data": ['views/custom_source_location.xml',
+             'views/res_config_settings.xml'
+
+             ],
+    "installable": True,
+}
