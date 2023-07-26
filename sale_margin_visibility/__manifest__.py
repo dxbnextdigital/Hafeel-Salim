78ecd9fd6d73e7e490902b41249d583cc270419d
Techfuge <105791931+erpsupporttecfuge@users.noreply.github.com>
2023-07-26 10:56:09 +0530

    [added] sales persons visibility

---
+++ sale_margin_visibility/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Visibility Margins in Sales Orders',
+    'version':'1.0',
+    'category': 'Sales/Sales',
+    'description': """Added the visibility of the user""",
+    'depends':['sale_management','sale_margin','sales_team'],
+    'data':['views/sale_margin_view.xml'],
+    'license': 'LGPL-3',
+}
