ef43cd4e58320a4eff8f39dd6670cdd273ecd153
Techfuge <105791931+erpsupporttecfuge@users.noreply.github.com>
2023-08-11 21:07:54 +0530

    user_specific_journal_on_invoice_payment default

--- user_specific_journal_on_invoice_payment/models/custom_source_location.py
+++ user_specific_journal_on_invoice_payment/models/custom_source_location.py
@@ -10,7 +10,11 @@ class AccountPaymentRegister(models.TransientModel):
 
     @api.depends('journal_id')
     def _compute_domain_payment_register(self):
+
         if self.env.user.journal_id:
+            for journal in  self.env.user.journal_id.id:
+                self.journal_id = journal
+                break
             self.journal_id_domain = json.dumps([('id', 'in', self.env.user.journal_id.mapped('id'))])
         else:
             self.journal_id_domain = json.dumps([('type', 'in', ('cash', 'bank'))])
