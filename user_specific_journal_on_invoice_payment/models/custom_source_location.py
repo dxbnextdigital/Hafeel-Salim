7688db644ae0a39322b08b996a7d477ba1c4ab43
Techfuge <105791931+erpsupporttecfuge@users.noreply.github.com>
2023-08-11 20:41:47 +0530

    test

---
+++ user_specific_journal_on_invoice_payment/models/custom_source_location.py
@@ -0,0 +1,17 @@
+from odoo import models, fields, api, _
+import json
+class AccountPaymentRegister(models.TransientModel):
+    _inherit = "account.payment.register"
+
+    journal_id_domain = fields.Char(compute="_compute_domain_payment_register",
+    readonly=True,
+    store=False,)
+
+
+    @api.depends('journal_id')
+    def _compute_domain_payment_register(self):
+        if self.env.user.journal_id:
+            self.journal_id_domain = json.dumps([('id', 'in', self.env.user.journal_id.mapped('id'))])
+        else:
+            self.journal_id_domain = json.dumps([('type', 'in', ('cash', 'bank'))])
+
