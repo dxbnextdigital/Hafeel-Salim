bab6cbb2c9a100e1f8e3c877965e29540a511b48
Techfuge <105791931+erpsupporttecfuge@users.noreply.github.com>
2023-08-11 21:28:28 +0530

    user_specific_journal_on_invoice_payment

--- user_specific_journal_on_invoice_payment/models/custom_source_location.py
+++ user_specific_journal_on_invoice_payment/models/custom_source_location.py
@@ -4,14 +4,25 @@ class AccountPaymentRegister(models.TransientModel):
     _inherit = "account.payment.register"
 
     journal_id_domain = fields.Char(compute="_compute_domain_payment_register",
-    readonly=True,
-    store=False,)
+    readonly=True)
+
+    @api.depends('can_edit_wizard', 'company_id')
+    def _compute_journal_id(self):
+        super()._compute_journal_id()
+        if self.env.user.journal_id:
+            self.journal_id = False
+
 
 
     @api.depends('journal_id')
     def _compute_domain_payment_register(self):
+
+
         if self.env.user.journal_id:
+
             self.journal_id_domain = json.dumps([('id', 'in', self.env.user.journal_id.mapped('id'))])
         else:
             self.journal_id_domain = json.dumps([('type', 'in', ('cash', 'bank'))])
+
+
 
