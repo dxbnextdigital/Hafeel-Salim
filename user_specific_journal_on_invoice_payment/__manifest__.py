b3717a5c78c7218801b909d76675a67b8c9f18b5
Techfuge <105791931+erpsupporttecfuge@users.noreply.github.com>
2023-08-11 20:33:02 +0530

    user_specific_journal_on_invoice_payment

---
+++ user_specific_journal_on_invoice_payment/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "User Specific Payment In Invoice",
+    "version": "14.0.1.0.1",
+    "category": "",
+    "website": "",
+    "depends": ['user_customization','account','web_domain_field'],
+    "data": ['views/res_config_settings.xml'  ],
+    "installable": True,
+}
