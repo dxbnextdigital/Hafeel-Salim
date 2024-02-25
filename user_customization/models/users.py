52f4292b3001931088bbe0f607611431cab5d69d
Techfuge <105791931+erpsupporttecfuge@users.noreply.github.com>
2024-02-25 09:41:11 +0400

    user_customization INACTIVE USERS RESTRICTION

--- user_customization/models/users.py
+++ user_customization/models/users.py
@@ -1,6 +1,13 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 from datetime import datetime, timedelta
+
+from odoo import models, fields
+
+
+
+
+
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
@@ -8,6 +15,6 @@ class ResUsers(models.Model):
         'custom.source.location',
         'Source'
     )
-
+    source_users_invoice_restriction = fields.Char(string="Invoice User Ids With (-)")
     journal_id = fields.Many2many('account.journal', domain=[('type', 'in', ('cash', 'bank'))])
 
